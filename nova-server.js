9b40e43e15e18c5581eedecd74312c3301b1674d
I-R-I-S-Nova <lookpromotionsuk@gmail.com>
2025-05-05 23:23:58 +0100

    Update nova-server.js

--- nova-server.js
+++ nova-server.js
@@ -4,7 +4,7 @@ const { SessionsClient } = require("@google-cloud/dialogflow-cx");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 10000;
 
 app.use(cors());
 app.use(bodyParser.json());
